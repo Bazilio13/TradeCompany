dcf9eb4740511536e05bf26e16028c8aae52912f
Vadim <vki-13@yandex.ru>
2021-05-27 16:15:50 +0300

    Added Order/OrderList DTOs

---
+++ TradeCompany_DAL/DTOs/OrderListsDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TradeCompany_DAL.DTOs
+{
+    public class OrderListDTO
+    {
+        public int ID { get; set; }
+        public int OrderID { get; set; }
+        public int ProductID { get; set; }
+        public float Price { get; set; }
+    }
+}
---
+++ TradeCompany_DAL/DTOs/OrdersDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TradeCompany_DAL.DTOs
+{
+    public class OrdersDTO
+    {
+        public int ID { get; set; }
+        public int ClientsID { get; set; }
+        public DateTime Datetime { get; set; }
+        public int AddressID { get; set; }
+        public string Comment { get; set; }
+        public List<OrderListDTO> OrderList { get; set; }
+    }
+}
