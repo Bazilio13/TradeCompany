97d90b0f99d0f8a3941b906896fbd4a064126fa8
Vadim <vki-13@yandex.ru>
2021-05-27 21:53:24 +0300

    upd

--- TradeCompany_DAL/DTOs/OrderListsDTO.cs
+++ TradeCompany_DAL/DTOs/OrderListsDTO.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace TradeCompany_DAL.DTOs
 {
-    class OrderListsDTO
+    public class OrderListDTO
     {
+        public int ID { get; set; }
+        public int OrderID { get; set; }
+        public int ProductID { get; set; }
+        public float Amount { get; set; }
+        public float Price { get; set; }
     }
 }
