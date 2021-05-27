7975f172a42a10e315068d2871c6759725157833
Nebo <kindersmail08@gmail.com>
2021-05-28 00:45:03 +0300

    Add  5 base Stored Procerures

---
+++ TradeCompany_DataBase/TradeCompany_DataBase/Stored Procedures/GetClientByID.sql
@@ -0,0 +1,6 @@
+﻿CREATE PROCEDURE [TradeCompany_DataBase].[GetClientByID]
+	@ClientID int
+AS
+	SELECT C.ID, C.Name, C.INN, C.E_Mail, C.Phone, C.CorporateBody, C.Type, C.LastOrderDate, C.Comment
+	from [Clients] as C
+	where C.ID = @ClientID
---
+++ TradeCompany_DataBase/TradeCompany_DataBase/Stored Procedures/UpdateClientByID.sql
@@ -0,0 +1,22 @@
+﻿CREATE PROCEDURE [TradeCompany_DataBase].[UpdateClientByID]
+	@ClientID int,
+	@Name nvarchar(255),
+	@INN int,
+	@Email nvarchar(100),
+	@Phone nvarchar(50),
+	@Comment nvarchar(500),
+	@CorporateBody binary,
+	@Type binary,
+	@LastOrderDate datetime
+AS
+	update [Clients]
+	set 
+	[Name] = @Name,
+	[INN] = @INN,
+	[E_Mail] = @Email,
+	[Phone] = @Phone,
+	[Comment] = @Comment,
+	[CorporateBody] = @CorporateBody,
+	[Type] = @Type,
+	[LastOrderDate] = @LastOrderDate
+	where Clients.ID = @ClientID
