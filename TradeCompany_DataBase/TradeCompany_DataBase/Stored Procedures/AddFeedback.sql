e3cd9f45e5ac109bee7f7d290024429478e69a34
Katerina <k.panteleewa@gmail.com>
2021-05-31 00:01:52 +0300

    Add Stored Procedure for Feedbacks

---
+++ TradeCompany_DataBase/TradeCompany_DataBase/Stored Procedures/AddFeedback.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE [dbo].[AddFeedback]
+@Datetime datetime,
+@Text nvarchar(1500),
+@ClientID int,
+@OrderID int
+As 
+insert into FeedBacks(Datetime,Text,ClientID,OrderID)
+values (@Datetime,@Text,@ClientID,@OrderID)
