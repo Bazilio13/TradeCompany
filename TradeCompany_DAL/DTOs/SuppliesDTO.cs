60f737dc96fe1fa45637c99881fcf7128b8c89f1
Andrei <godsmelchak2005@gmail.com>
2021-06-01 17:48:35 +0300

    DTO for Supplies and SupplyLists

---
+++ TradeCompany_DAL/DTOs/SuppliesDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TradeCompany_DAL.DTOs
+{
+    public class SuppliesDTO
+    {
+        public int ID { get; set; }
+        public DateTime DateTime { get; set; }
+    }
+}
