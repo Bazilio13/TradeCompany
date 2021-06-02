3037f7bd1a8be64ac4480095ee6e8020447baa0a
MariaMalyutina <merymal2696@gmail.com>
2021-06-03 00:07:20 +0300

    Added Products and ProductGroups stored procedures

---
+++ TradeCompany_DataBase/TradeCompany_DataBase/Stored Procedures/AddProduct.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [TradeCompany_DataBase].[AddProduct]
+	@Name nvarchar (255),
+	@StockAmount float,
+	@MeasureUnit int,
+	@MinPrice float,
+	@MaxPrice float,
+	@LastSupplyDate datetime
+AS
+	insert into Products (Name, StockAmount, MeasureUnit, MinPrice, MaxPrice, LastSupplyDate)
+	values (@Name, @StockAmount, @MeasureUnit, @MinPrice, @MaxPrice, @LastSupplyDate)
---
+++ TradeCompany_DataBase/TradeCompany_DataBase/Stored Procedures/GetProducts.sql
@@ -0,0 +1,6 @@
+﻿create procedure [TradeCompany_DataBase].[GetProducts] --вместе с группами
+as
+select P.ID, P.Name, P.StockAmount, P.MeasureUnit, P.MinPrice, P.MaxPrice, P.LastSupplyDate, PG.ID, PG.Name
+from Products as P
+left join [TradeCompany_DataBase].[Product_ProductGroups] as P_PG on P.ID = P_PG.ProductID
+left join [TradeCompany_DataBase].[ProductGroups] as PG on PG.ID = P_PG.ProductGroupID
