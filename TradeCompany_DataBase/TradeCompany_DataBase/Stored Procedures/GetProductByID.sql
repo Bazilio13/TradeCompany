ce15cd897ac81854d221a77e34a85c0a38b4435e
MariaMalyutina <merymal2696@gmail.com>
2021-06-03 01:13:33 +0300

    Changed products stored procedures and products table

--- TradeCompany_DataBase/TradeCompany_DataBase/Stored Procedures/GetProductByID.sql
+++ TradeCompany_DataBase/TradeCompany_DataBase/Stored Procedures/GetProductByID.sql
@@ -1,7 +1,7 @@
 ﻿create procedure [TradeCompany_DataBase].[GetProductByID]
 @ProductId int
 as
-select P.ID, P.Name, P.StockAmount, P.MeasureUnit, P.MinPrice, P.MaxPrice, P.LastSupplyDate, PG.ID, PG.Name
+select P.ID, P.Name, P.StockAmount, P.MeasureUnit, P.WholesalePrice, P.RetailPrice, P.LastSupplyDate, P.Description, P.Comments, PG.ID, PG.Name
 from Products as P
 left join [TradeCompany_DataBase].[Product_ProductGroups] as P_PG on P.ID = P_PG.ProductID
 left join [TradeCompany_DataBase].[ProductGroups] as PG on PG.ID = P_PG.ProductGroupID
--- TradeCompany_DataBase/TradeCompany_DataBase/Stored Procedures/UpdateProductByID.sql
+++ TradeCompany_DataBase/TradeCompany_DataBase/Stored Procedures/UpdateProductByID.sql
@@ -3,17 +3,23 @@
 	@Name nvarchar (255),
 	@StockAmount float,
 	@MeasureUnit int,
-	@MinPrice float,
-	@MaxPrice float,
-	@LastSupplyDate datetime
+	@WholesalePrice float,
+	@RetailPrice float,
+	@LastSupplyDate datetime,
+	@Description nvarchar (500),
+	@Comments nvarchar (500)
 AS
 	update [Products] 
 set 
 	Name = @Name,
 	StockAmount = @StockAmount,
 	MeasureUnit = @MeasureUnit,
-	MinPrice = @MinPrice,
-	MaxPrice = @MaxPrice,
-	LastSupplyDate = @LastSupplyDate
+	WholesalePrice = @WholesalePrice,
+	RetailPrice = @RetailPrice,
+	LastSupplyDate = @LastSupplyDate,
+	Description = @Description,
+	Comments = @Comments
 where 
 	ID = @ProductID
+
+
