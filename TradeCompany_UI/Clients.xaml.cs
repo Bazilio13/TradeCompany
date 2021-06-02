bbf19f0be58050bc486d003d9a45bd739d6eaa3d
Nastya <kushkoeva99@gmail.com>
2021-06-02 08:19:16 +0300

    add tables Client and Clients

---
+++ TradeCompany_UI/Clients.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TradeCompany_DAL;
+using TradeCompany_DAL.DTOs;
+
+
+namespace TradeCompany_UI
+{
+    /// <summary>
+    /// Interaction logic for Clients.xaml
+    /// </summary>
+    public partial class Clients : Page
+    {
+        ClientsData clientsData = new ClientsData(@"Persist Security Info=False;User ID=DevEd;Password=qqq!11;Initial Catalog=Sandbox.Test;Server=80.78.240.16");
+
+        public Clients()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            List<ClientDTO> listClients = new List<ClientDTO>();
+
+            DataClients.ItemsSource = clientsData.GetClients();
+        }
+    }
+}
