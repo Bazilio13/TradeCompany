72d5412379194da4c4ccf96d33b0bd7781d510c9
Andrei <godsmelchak2005@gmail.com>
2021-06-03 19:29:19 +0300

    Fixed

--- TradeCompany_DataBase/TradeCompany_DataBase/Stored Procedures/DeleteGroupFromProductByProductID.sql
+++
@@ -1,7 +0,0 @@
-﻿create procedure [TradeCompany_DataBase].DeleteGroupFromProduct
-@ProductID int, 
-@ProductGroupId int
-as
-delete from [Product_ProductGroups]
-where Product_ProductGroups.ProductGroupID = @ProductGroupID 
-AND Product_ProductGroups.ProductID = @ProductID 
