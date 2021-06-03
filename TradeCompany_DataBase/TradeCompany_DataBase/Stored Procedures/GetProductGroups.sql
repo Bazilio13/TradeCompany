68a01d52a2b3a55ff659ea204122dfd58d5eef26
MariaMalyutina <merymal2696@gmail.com>
2021-06-03 15:40:08 +0300

    Update stored procedures for products

--- TradeCompany_DataBase/TradeCompany_DataBase/Stored Procedures/GetProductGroups.sql
+++ TradeCompany_DataBase/TradeCompany_DataBase/Stored Procedures/GetProductGroups.sql
@@ -1,6 +1,7 @@
 ﻿CREATE PROCEDURE [TradeCompany_DataBase].[GetProductGroups]
 AS
-	select PG.ID, PG.Name, P.ID, P.Name, P.StockAmount, P.MeasureUnit, P.WholesalePrice, P.RetailPrice, P.LastSupplyDate, P.Description, P.Comments
-	from ProductGroups as PG
+	select PG.ID, PG.Name, P.ID, P.Name, P.StockAmount, MU.Name as [MeasureUnitName], P.WholesalePrice, P.RetailPrice, P.LastSupplyDate, P.Description, P.Comments
+	from [TradeCompany_DataBase].ProductGroups as PG
 	left join [TradeCompany_DataBase].[Product_ProductGroups] as P_PG on PG.ID = P_PG.ProductGroupID
 	left join [TradeCompany_DataBase].[Products] as P on P.ID = P_PG.ProductID
+	left join [TradeCompany_DataBase].MeasureUnits as MU on MU.ID = P.MeasureUnit
