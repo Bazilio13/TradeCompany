91724209ebac35b4441cc8a0438dfbd970512103
Andrei <godsmelchak2005@gmail.com>
2021-06-04 01:59:44 +0300

    FIXED By Anton

--- TradeCompany_BLL/Models/ProductBaseModel.cs
+++ TradeCompany_BLL/Models/ProductBaseModel.cs
@@ -11,7 +11,7 @@ namespace TradeCompany_BLL.Models
         public int ID { get; set; }
         public string Name { get; set; }
         public float StockAmount { get; set; }
-        public int MeasureUnitName { get; set; }
+        public string MeasureUnitName { get; set; }
         public float WholesalePrice { get; set; }
         public float RetailPrice { get; set; }
         public DateTime? LastSupplyDate { get; set; }
