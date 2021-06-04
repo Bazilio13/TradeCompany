6b018527c57557b7fadb4195d184de3d8ae1b4ec
Nebo <kindersmail08@gmail.com>
2021-06-04 03:09:51 +0300

    Add GetClientByParam

---
+++ TradeCompany_DataBase/TradeCompany_DataBase/Stored Procedures/GetClientsByParams.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE [TradeCompany_DataBase].[GetClientsByParams]
+	@Person binary,
+	@Sale binary,
+	@MinData DateTime,
+	@MaxData DateTime
+AS
+	Select C.ID, C.Name, C.INN, C.E_Mail, C.Phone, C.ContactPerson, C.CorporateBody, c.Type,C.LastOrderDate, C.Comment
+	from [Clients] as C
+	Where 
+	(@Person IS NULL OR C.Type = @Person ) AND
+	(@Sale IS NULL OR C.CorporateBody = @Sale ) AND
+	(@MinData IS NULL OR C.LastOrderDate >= @MinData) AND
+	(@MaxData IS NULL OR C.LastOrderDate <= @MaxData) 
