c445408a97d226e7f7d1b628a3b83dbc0c3c8ccc
Nebo <kindersmail08@gmail.com>
2021-06-05 18:09:12 +0300

    Add stored prosedures WishesList

---
+++ TradeCompany_DataBase/TradeCompany_DataBase/Stored Procedures/GetWishesListByClientID.sql
@@ -0,0 +1,7 @@
+﻿CREATE PROCEDURE [TradeCompany_DataBase].[GetWishesListByClientID]
+	@ClientID int
+AS
+	select P.Name, P.ID
+	from TradeCompany_DataBase.Wishes as W
+	left join [TradeCompany_DataBase].Products as P on W.ProductsID = P.ID
+	where W.ClientsID = @ClientID
