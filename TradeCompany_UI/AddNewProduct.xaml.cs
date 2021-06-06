f0b5ffbc28a972bfc43498d40ec2c94b66dd12d9
MariaMalyutina <merymal2696@gmail.com>
2021-06-07 02:12:33 +0300

    AddNewProduct page Design fix

--- TradeCompany_UI/AddNewProduct.xaml.cs
+++ TradeCompany_UI/AddNewProduct.xaml.cs
@@ -36,66 +36,23 @@ namespace TradeCompany_UI
             {
                 Category.Items.Add(productGroupName[i].Name);
             }
+            CreationDate.Text = DateTime.Now.ToString();
+            Button_Save.IsEnabled = false;
         }
 
         private void Button_Save_Click(object sender, RoutedEventArgs e)
         {
-            if (CheckingFieldsForData())
-            {
-                _product.Name = Name_Text.Text;
-                _product.StockAmount = (float)Convert.ToDouble(Text_StockAmount.Text);
-                _product.MeasureUnit = 1; //добавить подтягивание из таблицы MeasureUnit
-                _product.RetailPrice = (float)Convert.ToDouble(Text_RetailPrice.Text);
-                _product.WholesalePrice = (float)Convert.ToDouble(Text_WholesalePrice.Text);
-                _product.Description = Text_Description.Text;
-                _product.Comments = Text_Comments.Text;
-                _product.LastSupplyDate = DateTime.Now;
-                _products.AddNewProduct(_product);
-            }
-            else
-            {
-                MessageBox.Show("Вы ввели не все данные");
-            }
-        }
+            _product.Name = Name_Text.Text;
+            _product.StockAmount = (float)Convert.ToDouble(Text_StockAmount.Text);
+            _product.MeasureUnit = 1; //добавить подтягивание из таблицы MeasureUnit
+            _product.RetailPrice = (float)Convert.ToDouble(Text_RetailPrice.Text);
+            _product.WholesalePrice = (float)Convert.ToDouble(Text_WholesalePrice.Text);
+            _product.Description = Text_Description.Text;
+            _product.Comments = Text_Comments.Text;
+            _product.LastSupplyDate = DateTime.Now;
+            _products.AddNewProduct(_product);
 
-
-        private bool CheckingFieldsForData()
-        {
-            bool IsValid = true;
-            if (Name_Text.Text == "")
-            {
-                Name_Text.Background = Brushes.Pink;
-                IsValid = false;
-            }
-
-            if (Text_RetailPrice.Text == "")
-            {
-                Text_RetailPrice.Background = Brushes.Pink;
-                IsValid = false;
-            }
-
-            if (Text_WholesalePrice.Text == "")
-            {
-                Text_WholesalePrice.Background = Brushes.Pink;
-                IsValid = false;
-            }
-
-            if (Text_StockAmount.Text == "")
-            {
-                Text_StockAmount.Background = Brushes.Pink;
-                IsValid = false;
-            }
-            if (_product.MeasureUnit is null)
-            {
-                MeasureUnit.Background = Brushes.Pink;
-                IsValid = false;
-            }
-            if (_product.Groups is null)
-            {
-                Category.Background = Brushes.Pink;
-                IsValid = false;
-            }
-            return IsValid;
+            frame.Content = new ProductCatalog();
         }
 
         private void Name_Text_TextChanged(object sender, TextChangedEventArgs e)
@@ -104,6 +61,7 @@ namespace TradeCompany_UI
             {
                 Name_Text.Background = Brushes.White;
             }
+            EnableSaveButton();
         }
 
         private void Text_RetailPrice_TextChanged(object sender, TextChangedEventArgs e)
@@ -113,6 +71,8 @@ namespace TradeCompany_UI
                 Text_RetailPrice.Background = Brushes.White;
             }
             InputValidation(Text_RetailPrice);
+
+            EnableSaveButton();
         }
 
         private void Text_WholesalePrice_TextChanged(object sender, TextChangedEventArgs e)
@@ -122,6 +82,8 @@ namespace TradeCompany_UI
                 Text_WholesalePrice.Background = Brushes.White;
             }
             InputValidation(Text_WholesalePrice);
+
+            EnableSaveButton();
         }
 
         private void Text_StockAmount_TextChanged(object sender, TextChangedEventArgs e)
@@ -131,6 +93,70 @@ namespace TradeCompany_UI
                 Text_StockAmount.Background = Brushes.White;
             }
             InputValidation(Text_StockAmount);
+
+            EnableSaveButton();
+        }
+
+
+        private void Buton_Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            frame.Content = new ProductCatalog();
+        }
+
+        private void Category_DropDownClosed(object sender, EventArgs e)
+        {
+
+            EnableSaveButton();
+        }
+
+        private void MeasureUnit_DropDownClosed(object sender, EventArgs e)
+        {
+            EnableSaveButton();
+        }
+
+        private bool CheckingFieldsForData()
+        {
+            bool IsValid = true;
+            if (Name_Text.Text == "")
+            {
+                IsValid = false;
+            }
+
+            if (Text_RetailPrice.Text == "")
+            {
+                IsValid = false;
+            }
+
+            if (Text_WholesalePrice.Text == "")
+            {
+                IsValid = false;
+            }
+
+            if (Text_StockAmount.Text == "")
+            {
+                IsValid = false;
+            }
+            if (_product.MeasureUnit is null)
+            {
+                IsValid = false;
+            }
+            if (_product.Groups is null)
+            {
+                IsValid = false;
+            }
+            return IsValid;
+        }
+
+        private void EnableSaveButton()
+        {
+            if (CheckingFieldsForData())
+            {
+                Button_Save.IsEnabled = true;
+            }
+            else
+            {
+                Button_Save.IsEnabled = false;
+            }
         }
 
         private void InputValidation(TextBox textbox)
@@ -138,7 +164,7 @@ namespace TradeCompany_UI
             textbox.Text = Regex.Replace(textbox.Text, @"[.]+", ",");
             textbox.Text = Regex.Replace(textbox.Text, @"[^0-9,.]+", "");
             textbox.SelectionStart = textbox.Text.Length;
-            if(textbox.Text != "")
+            if (textbox.Text != "")
             {
                 try
                 {
@@ -150,16 +176,6 @@ namespace TradeCompany_UI
                     MessageBox.Show("Неверный ввод");
                 }
             }
-        }
-
-        private void Buton_Cancel_Click(object sender, RoutedEventArgs e)
-        {
-            frame.Content = new ProductCatalog();
-        }
-
-        private void Category_DropDownClosed(object sender, EventArgs e)
-        {
-
         }
     }
 }
