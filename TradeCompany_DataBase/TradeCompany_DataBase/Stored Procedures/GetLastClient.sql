6429e20d7a3b8baab22f59f71ac7787fc02227b9
Nebo <kindersmail08@gmail.com>
2021-06-06 17:38:33 +0300

    add GetlastClient

---
+++ TradeCompany_DataBase/TradeCompany_DataBase/Stored Procedures/GetLastClient.sql
@@ -0,0 +1,5 @@
+﻿CREATE PROCEDURE [TradeCompany_DataBase].[GetLastClient]
+AS
+	Select C.ID, C.Name, C.INN, C.E_Mail, C.Phone, C.ContactPerson, C.CorporateBody, c.Type,C.LastOrderDate, C.Comment
+	FROM TradeCompany_DataBase.Clients as C
+	WHERE id=(SELECT max(id) FROM TradeCompany_DataBase.Clients)
