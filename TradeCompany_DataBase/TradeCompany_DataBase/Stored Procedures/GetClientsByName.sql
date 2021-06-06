a5c4e8db43f9788fa97439189d36af0ea4af2086
Nebo <kindersmail08@gmail.com>
2021-06-06 13:48:03 +0300

    Fix GetClientsByName

--- TradeCompany_DataBase/TradeCompany_DataBase/Stored Procedures/GetClientsByName.sql
+++ TradeCompany_DataBase/TradeCompany_DataBase/Stored Procedures/GetClientsByName.sql
@@ -2,5 +2,5 @@
 	@PartOfTheName nvarchar(50)
 AS
 	select * from TradeCompany_DataBase.Clients
-	where Name LIKE '%'+@PartOfTheName+'%'
+	where Name LIKE '%'+@PartOfTheName+'%' or INN LIKE @PartOfTheName+'%' or Phone LIKE @PartOfTheName+'%' or  E_Mail LIKE @PartOfTheName+'%'
 
