0f3c6962771a18964184c9b520ed1aefcc1444d6
Nebo <kindersmail08@gmail.com>
2021-06-06 19:30:19 +0300

    fix GetOrderByClientID

--- TradeCompany_DataBase/TradeCompany_DataBase/Stored Procedures/GetOrderByClientID.sql
+++ TradeCompany_DataBase/TradeCompany_DataBase/Stored Procedures/GetOrderByClientID.sql
@@ -3,6 +3,6 @@
 AS
 	SELECT O.ID, O.ClientsID, A.Address, O.DateTime, O.Comment
 	from TradeCompany_DataBase.Orders as O
-	left join TradeCompany_DataBase.Addresses as A on A.ClientID = o.AddressID
+	Left join TradeCompany_DataBase.Addresses as A on A.ID = o.AddressID
 	where ClientsID = @ID
 	order by o.DateTime desc
