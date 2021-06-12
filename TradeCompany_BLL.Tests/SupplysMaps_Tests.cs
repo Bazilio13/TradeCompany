fc21af2269bb296a4707a24de5a922db84c19c67
Vadim <vki-13@yandex.ru>
2021-06-13 01:07:54 +0300

    upd

--- TradeCompany_BLL.Tests/SupplysMaps_Tests.cs
+++ TradeCompany_BLL.Tests/SupplysMaps_Tests.cs
@@ -1,13 +1,23 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TradeCompany_BLL.SypplysMaps;
 
 namespace TradeCompany_BLL.Tests
 {
     public class SupplysMaps_Tests
     {
+        SupplysMaps _map;
+
+        [SetUp]
+        public void Setup()
+        {
+            _map = new SupplysMaps();
+        }
+
 
     }
 }
