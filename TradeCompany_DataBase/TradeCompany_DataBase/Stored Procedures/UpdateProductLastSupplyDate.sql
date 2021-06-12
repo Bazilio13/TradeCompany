3a815cc8a5614b0dcec084add39b0daae5ed2840
Vadim <vki-13@yandex.ru>
2021-06-12 17:09:25 +0300

    Add updating lastSupplyDate

---
+++ TradeCompany_DataBase/TradeCompany_DataBase/Stored Procedures/UpdateProductLastSupplyDate.sql
@@ -0,0 +1,18 @@
+﻿CREATE PROCEDURE [TradeCompany_DataBase].[UpdateProductLastSupplyDate]
+		@SupplyID int
+AS
+	UPDATE  P
+SET
+    P.LastSupplyDate = LS.LastSupply  
+FROM
+(select P.ID, P.LastSupplyDate from
+		[TradeCompany_DataBase].Products AS P
+	JOIN [TradeCompany_DataBase].SupplyLists AS SL ON SL.ProductID = P.ID
+	join [TradeCompany_DataBase].Supplies as S ON SL.SupplyID = S.ID
+	where S.ID = @SupplyID) P 
+	left join
+(select P.ID, Max(S.DateTime) as LastSupply from
+    [TradeCompany_DataBase].Products AS P
+    JOIN [TradeCompany_DataBase].SupplyLists AS SL ON SL.ProductID = P.ID
+	join [TradeCompany_DataBase].Supplies as S ON SL.SupplyID = S.ID
+	group by P.ID, P.LastSupplyDate) LS on P.ID = LS.ID
