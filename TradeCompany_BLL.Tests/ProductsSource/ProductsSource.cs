eae5eb21103839603f4a524464182e3191f99223
Nastya <kushkoeva99@gmail.com>
2021-06-12 22:46:24 +0300

    add ProductForOrderDTO source

--- TradeCompany_BLL.Tests/ProductsSource/ProductsSource.cs
+++ TradeCompany_BLL.Tests/ProductsSource/ProductsSource.cs
@@ -1,12 +1,44 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TradeCompany_BLL.Models;
+using TradeCompany_DAL.DTOs;
 
 namespace TradeCompany_BLL.Tests.ProductsSource
 {
     class ProductsSource
     {
+        public class Map_ProductsForOrderDTO_To_ProductsForOrderModelSource : IEnumerable
+        {
+            public IEnumerator GetEnumerator()
+            {
+                yield return new object[]
+                {
+                    new List<ProductForOrderDTO>{
+                        ProductID = 1,
+                        Name = "Мыло",
+                        Amount = 20,
+                        Price = 30,
+                        MeasureUnit = 5
+                        ProductGroupName = "Хоз"
+                     },
+                    new List<ProductsForOrderModel>{ }
+                };
+
+                yield return new object[]
+                {
+
+                };
+
+                yield return new object[]
+                {
+
+                };
+            }
+        }
+
     }
 }
