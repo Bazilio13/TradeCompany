bf9a28b95227a78158dda9b2e72ddbaa415b4df8
Nebo <kindersmail08@gmail.com>
2021-06-14 00:01:42 +0300

    AddTests

---
+++ TradeCompany_BLL.Tests/StatisticMapSource/MapStatisticsProductsDTOToStatisticsProductsModel_Source.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TradeCompany_BLL.Models;
+using TradeCompany_BLL.Tests.Mocks;
+using TradeCompany_DAL.DTOs;
+
+namespace TradeCompany_BLL.Tests.StatisticMapSource
+{
+    class MapStatisticsProductsDTOToStatisticsProductsModel_Source:IEnumerable
+    {
+
+        public IEnumerator GetEnumerator()
+        {
+            yield return new object[] { StatisticsDTOMock.ProductDTO, StatisticsModelMock.ProductModel };
+            yield return new object[] { new List<StatisticsProductDTO>() { StatisticsDTOMock.ProductDTO[1], StatisticsDTOMock.ProductDTO[1] } ,
+                new List<StatisticsProductModel>(){ StatisticsModelMock.ProductModel[1], StatisticsModelMock.ProductModel[1] }  };
+            yield return new object[] { new List<StatisticsProductDTO>() { StatisticsDTOMock.ProductDTO[1], StatisticsDTOMock.ProductDTO[0] } ,
+                new List<StatisticsProductModel>(){ StatisticsModelMock.ProductModel[1], StatisticsModelMock.ProductModel[0] }  };
+
+        }
+
+
+    }
+}
