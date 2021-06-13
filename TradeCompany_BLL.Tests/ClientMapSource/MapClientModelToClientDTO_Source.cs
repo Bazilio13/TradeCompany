6cb6e5cf0c636535e4fd2c57f44cf31f27b0e8f6
Nebo <kindersmail08@gmail.com>
2021-06-13 22:44:59 +0300

    Add 2 tests

---
+++ TradeCompany_BLL.Tests/ClientMapSource/MapClientModelToClientDTO_Source.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TradeCompany_BLL.Tests.Mocks;
+
+namespace TradeCompany_BLL.Tests.ClientMapSource
+{
+    class MapClientModelToClientDTO_Source:IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            yield return new object[] { ClientListModelMock.Model[0], ClientListDTOMock.DTOsFromDB[0]};
+            yield return new object[] { ClientListModelMock.Model[1], ClientListDTOMock.DTOsFromDB[1] };
+            yield return new object[] { ClientListModelMock.Model[2], ClientListDTOMock.DTOsFromDB[2] };
+        }
+    }
+
+}
+
--- TradeCompany_BLL/MapsModelToDTO.cs
+++ TradeCompany_BLL/MapsModelToDTO.cs
@@ -12,24 +12,24 @@ namespace TradeCompany_BLL
 {
     public class MapsModelToDTO
     {
+
         public ClientDTO MapClientModelToClientDTO(ClientModel clientModel)
         {
             var config = new MapperConfiguration(cfg => cfg.CreateMap<ClientModel, ClientDTO>());
             Mapper mapper = new Mapper(config);
             ClientDTO clientDTO = mapper.Map<ClientDTO>(clientModel);
-            ClientsData data = new ClientsData(@"Persist Security Info=False;User ID=DevEd;Password=qqq!11;Initial Catalog=Sandbox.Test;Server=80.78.240.16");
             return clientDTO;
         }
 
         public List<WishDTO> MapWishListModelToWishListDTO(List<WishModel> wishListModel)
         {
-            ClientsData data = new ClientsData(@"Persist Security Info=False;User ID=DevEd;Password=qqq!11;Initial Catalog=Sandbox.Test;Server=80.78.240.16");
-
             var config = new MapperConfiguration(cfg => cfg.CreateMap<WishModel, WishDTO>());
             Mapper mapper = new Mapper(config);
             List<WishDTO> wishListDTO = mapper.Map<List<WishDTO>>(wishListModel);
             return wishListDTO;
         }
+
+
 
 
 
