0db24067a61edc08fb02e5255945c1abcc9c6b04
Vadim <vki-13@yandex.ru>
2021-06-14 16:02:00 +0300

    add updateing clients lastorder

--- TradeCompany_DataBase/TradeCompany_DataBase/Stored Procedures/AddOrder.sql
+++ TradeCompany_DataBase/TradeCompany_DataBase/Stored Procedures/AddOrder.sql
@@ -8,3 +8,17 @@ AS
 	values (@ClientsID, @Datetime,  @AddressID, @Comment)
 
 	Select SCOPE_IDENTITY()
+
+		UPDATE  B
+SET
+     B.LastOrderDate = D.LastOrder
+FROM
+(select C.ID, C.LastOrderDate from
+		[TradeCompany_DataBase].Clients AS C
+		where C.ID = @ClientsID ) B
+		left join
+	(select C.ID,  max(O.DateTime) as LastOrder from
+		[TradeCompany_DataBase].Clients AS C
+	join [TradeCompany_DataBase].Orders as O ON O.ClientsID = C.ID
+	where C.ID = @ClientsID 
+	group by C.ID, C.LastOrderDate) D on B.ID = D.ID
--- TradeCompany_DataBase/TradeCompany_DataBase/Stored Procedures/UpdateOrder.sql
+++ TradeCompany_DataBase/TradeCompany_DataBase/Stored Procedures/UpdateOrder.sql
@@ -14,3 +14,17 @@ AS
 	from [TradeCompany_DataBase].Orders as O
 
 where o.ID = @ID
+
+UPDATE  B
+SET
+     B.LastOrderDate = D.LastOrder
+FROM
+(select C.ID, C.LastOrderDate from
+		[TradeCompany_DataBase].Clients AS C
+		where C.ID = @ClientsID ) B
+		left join
+	(select C.ID,  max(O.DateTime) as LastOrder from
+		[TradeCompany_DataBase].Clients AS C
+	join [TradeCompany_DataBase].Orders as O ON O.ClientsID = C.ID
+	where C.ID = @ClientsID 
+	group by C.ID, C.LastOrderDate) D on B.ID = D.ID
